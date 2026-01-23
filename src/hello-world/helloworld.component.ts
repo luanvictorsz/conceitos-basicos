e2eb542e79c9a8c509a0c823f5388dc32986a421
victor souza <luanvitorlima2017@gmail.com>
2026-01-23 12:29:40 -0300

    feat: estrutura de um componente angular e utilizacao no sistema

---
+++ src/hello-world/helloworld.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//declaração do componente
+@Component({
+    selector: 'hello-world',
+    template: '<h1>hello-world</h1>',
+})
+
+//classe do meu componente
+export class HelloWorldComponent{
+
+}
