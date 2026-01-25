a77ec322b86e680d4e0b6c0f60425108171b39e7
victor souza <luanvitorlima2017@gmail.com>
2026-01-24 23:22:42 -0300

    feat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { HelloWorldComponent } from "../hello-world/helloworld.component";
-import { ComponenteGeradoComponent } from "../componente-gerado/componente-gerado.component";
+// import { HelloWorldComponent } from "../hello-world/helloworld.component";
+// import { ComponenteGeradoComponent } from "../componente-gerado/componente-gerado.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWorldComponent, ComponenteGeradoComponent],
+  imports: [  RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
