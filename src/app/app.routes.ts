c9e3bc979b50ac6e962f6a8dd3498d3e954b3561
victor souza <luanvitorlima2017@gmail.com>
2026-01-29 22:07:51 -0300

    feat: criando tabela da lista de compras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CalculadoraComponent } from '../calculadora/calculadora.component';
+import { ListaComprasComponent } from '../lista-compras/lista-compras.component';
 
 export const routes: Routes = [
-    { path: 'calculadora', component: CalculadoraComponent }
+    { path: 'calculadora', component: CalculadoraComponent },
+    { path: 'lista-compras', component: ListaComprasComponent }
 ];
