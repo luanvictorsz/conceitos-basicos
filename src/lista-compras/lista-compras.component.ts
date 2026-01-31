aeb6eff32e4b7de159ea81b42b7cd95e4d14ee4d
victor souza <luanvitorlima2017@gmail.com>
2026-01-30 21:37:33 -0300

    feat: adicionand logica do formulário, adicionando item no console

--- src/lista-compras/lista-compras.component.ts
+++ src/lista-compras/lista-compras.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.css'
 })
 export class ListaComprasComponent {
+  item: string = "";
 
+  adicionarItem(){
+    console.log('item adicionado: ' + this.item);
+    this.item = '';
+  }
 }
